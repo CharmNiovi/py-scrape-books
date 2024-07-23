2dd94158f1c1cb1106c920be82fff582ebcc72c6
charm <charmniovi@tutanota.com>
2024-07-23 13:07:11 +0300

    solution

--- shelf/spiders/books.py
+++ shelf/spiders/books.py
@@ -3,6 +3,14 @@ import re
 import scrapy
 from scrapy.http import Response
 
+RATING = {
+    "One": 1,
+    "Two": 2,
+    "Three": 3,
+    "Four": 4,
+    "Five": 5
+}
+
 
 class BooksSpider(scrapy.Spider):
     name = "books"
@@ -19,36 +27,53 @@ class BooksSpider(scrapy.Spider):
         if next_href := response.css("li.next a::attr(href)").get():
             yield response.follow(next_href, callback=self.parse)
 
-    @staticmethod
-    def parse_book_detail(response: Response) -> dict:
+    def parse_book_detail(self, response: Response) -> dict:
         product_detail = dict(
             zip(
                 response.css("table tr th::text").getall(),
                 response.css("table tr td::text").getall()
             )
         )
-        rating = {
-            "One": 1,
-            "Two": 2,
-            "Three": 3,
-            "Four": 4,
-            "Five": 5
-        }
+
         yield {
-            "title": response.css(".product_main h1::text").get(),
-            "category": product_detail.get("Product Type"),
-            "upc": product_detail.get("UPC"),
-            "price": response.css(
-                ".product_main p.price_color::text"
-            ).get()[1:],
-            "rating": rating.get(
-                response.css("p.star-rating::attr(class)").get().split()[-1]
-            ),
-            "description": response.css(
-                "#product_description + p::text"
-            ).get(),
-            "amount_in_stock": int(re.sub(
-                "\D", "",  # NOQA W605
-                product_detail.get("Availability")
-            )),
+            "title": self.get_title(response),
+            "category": self.get_category(product_detail),
+            "upc": self.get_upc(product_detail),
+            "price": self.get_price(response),
+            "rating": self.get_rating(response),
+            "description": self.get_description(response),
+            "amount_in_stock": self.get_amount_in_stock(product_detail),
         }
+
+    @staticmethod
+    def get_title(response: Response) -> str:
+        return response.css(".product_main h1::text").get()
+
+    @staticmethod
+    def get_category(product_detail: dict) -> str:
+        return product_detail.get("Product Type")
+
+    @staticmethod
+    def get_upc(product_detail: dict) -> str:
+        return product_detail.get("UPC")
+
+    @staticmethod
+    def get_price(response: Response) -> str:
+        return response.css(".product_main p.price_color::text").get()[1:]
+
+    @staticmethod
+    def get_rating(response: Response) -> int | None:
+        return RATING.get(
+            response.css("p.star-rating::attr(class)").get().split()[-1]
+        )
+
+    @staticmethod
+    def get_description(response: Response) -> str:
+        return response.css("#product_description ~ p::text").get()
+
+    @staticmethod
+    def get_amount_in_stock(product_detail: dict) -> int:
+        return int(re.sub(
+            "\D", "",  # NOQA W605
+            product_detail.get("Availability")
+        ))
